7f88bd8f23b403319e9b04074bc3b9927e0da9b3
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2025-01-10 00:18:02 +0400

    Add files via upload

--- module_9_test.py
+++ module_9_test.py
@@ -18,26 +18,43 @@
 # my_func = get_russian_names
 # print(my_func())
 # print(my_func.__name__)
-def adder(args):
-    res = 0
-    for number in args:
-        res += number
-    return res
-
-
-def multiplier(args):
-    res = 1
-    for number in args:
-        res *= number
-    return res
-
-
-def process_numbers(numbers, function):
-    result = function(numbers)
-    print(f'Получилось {result}')
-
+# def adder(args):
+#     res = 0
+#     for number in args:
+#         res += number
+#     return res
+#
+#
+# def multiplier(args):
+#     res = 1
+#     for number in args:
+#         res *= number
+#     return res
+#
+#
+# def process_numbers(numbers, function):
+#     result = function(numbers)
+#     print(f'Получилось {result}')
+#
+#
+# my_numbers = [3, 1, 4, 1, 5, 9, 2, 6]
+#
+# process_numbers(numbers=my_numbers, function=adder)
+# process_numbers(numbers=my_numbers, function=multiplier)
+# def mul_dy_2(x):
+#     return x * 2
+#
+#
+# my_numbers = [3, 1, 4, 1, 5, 9, 2, 6]
+#
+# result = map(mul_dy_2, my_numbers)
+# print(result)
+# print(list(result))
+def is_odd(x):
+    return x % 2
 
 my_numbers = [3, 1, 4, 1, 5, 9, 2, 6]
 
-process_numbers(numbers=my_numbers, function=adder)
-process_numbers(numbers=my_numbers, function=multiplier)
+result = filter(is_odd, my_numbers)
+print(result)
+print(list(result))
