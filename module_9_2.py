16f56404808c414dd579494b7f854935e87c963b
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2025-01-12 14:57:59 +0300

    Add files via upload

--- module_9_2.py
+++ module_9_2.py
@@ -6,6 +6,5 @@ first_result = [len(x) for x in first_strings if len(x) > 5]
 print(first_result)
 second_result = [(f_s, s_s) for f_s in first_strings for s_s in second_strings if len(f_s) == len(s_s)]
 print(second_result)
-third_result = [{well: len(well)} for well in first_strings + second_strings if len(well) % 2 == 0]
+third_result = {well: len(well) for well in first_strings + second_strings if len(well) % 2 == 0}
 print(third_result)
-
